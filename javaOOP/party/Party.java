d804305626b2d974ddd67e7d55c6da237b568462
nizamra <nizamra_95@hotmail.com>
2021-08-27 22:42:43 +0300

    Simple Guessing Game with three players

--- javaOOP/party/Party.java
+++ javaOOP/party/Party.java
@@ -1,8 +1,11 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
-class Party{
-    public void bouildInvite(){
+class Party {
+    public void bouildInvite() {
         Frame f = new Frame();
         Label l = new Label("Party at Main");
         Button b = new Button("yeees");
@@ -11,14 +14,21 @@ class Party{
         p.add(l);
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         // bouildInvite();
+        System.out.print("System Here!");
         try {
-            readTheFile("myFile.txt");
-            }
-        catch(FileNotFoundException ex) {
+            Path fileName = Path.of("demo.txt");
+            // String content = "hello world !!";
+            // Files.writeString(fileName, content);
+
+            String actual = Files.readString(fileName);
+            System.out.println(actual);
+            // readTheFile("myFile.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
             System.out.print("File not found.");
-            }
+        }
     }
 
 }
